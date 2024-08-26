5e04a94601d4b4e3ae4fe7c991ec4421408919a6
Ahmet Kutay Karacair <akkaracair@gmail.com>
2024-08-26 22:22:46 +0300

    after rebase added logout flow

--- routes/auth/index.ts
+++ routes/auth/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import registerUser from './register';
 import loginUser from './login';
 import verificationUser from "./verificationUser";
+import logoutUser from './logout';
 
 const router = express.Router();
 
 router.use('/register', registerUser);
 router.use('/login', loginUser);
 router.use('/verification', verificationUser);
+router.use('/logout', logoutUser);
 
 export default router;
